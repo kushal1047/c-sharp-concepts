8880d8a63126332b626a027b4d093ee5a0140270
Kushal Niraula <kushal463334@gmail.com>
2024-08-05 18:39:00 +1000

    Declare a 3D array and access its elements

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+170119d82f466c42aebd074982ad6f4d920dc4a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3020af9be4edad6c72e815d26d6e6043bbda98")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
